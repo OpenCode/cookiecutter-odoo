323a97fa4a4051265d790e214c443936ef27d6fd
Francesco Apruzzese <cescoap@gmail.com>
2015-09-12 11:07:15 +0200

    [ADD] Base structure for odoo module

---
+++ {{cookiecutter.module_name}}/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © <{{cookiecutter.year}}> <{{cookiecutter.author}}>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "{{cookiecutter.project_name}}",
+    "summary": "{{cookiecutter.project_short_description}}",
+    "version": "{{cookiecutter.version}}",
+    "category": "{{cookiecutter.category}}",
+    "website": "https://odoo-community.org/",
+    "author": "{{cookiecutter.author}}, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "base",
+    ],
+    "data": [
+    ],
+    "demo": [
+    ],
+}
+
